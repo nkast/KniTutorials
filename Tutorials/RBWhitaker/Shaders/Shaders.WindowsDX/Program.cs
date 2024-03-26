75101f1212e4d72b249f232f7fb37c44b4fbd147
Nikos Kastellanos <nkastellanos@gmail.com>
2024-03-26 17:59:51 +0200

    Shaders KNI projects

---
+++ Tutorials/RBWhitaker/Shaders/Shaders.WindowsDX/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shaders
+{
+#if  WINDOWSDX || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ShadersGame())
+                game.Run();
+        }
+    }
+#endif
+}
--- Tutorials/RBWhitaker/Shaders/ShadersContent/Effects/NormalMap.fx
+++ Tutorials/RBWhitaker/Shaders/ShadersContent/Effects/NormalMap.fx
@@ -1,3 +1,4 @@
+
 float4x4 World;
 float4x4 View;
 float4x4 Projection;
